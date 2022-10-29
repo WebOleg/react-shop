0f8d0cb7f87cbc465585c32c3d57b41b72f12efc
oleg minenko <olegminenko2005gmail.com@MacBook-Pro-oleg.local>
2022-10-29 17:08:26 +0300

    add new item functional

--- src/components/Products/ProductsListItem.js
+++ src/components/Products/ProductsListItem.js
@@ -1,4 +1,10 @@
-import { Card, CardActions, CardContent, Button } from '@mui/material'
+import {
+    Card,
+    CardActions,
+    CardContent,
+    Button,
+    TextField,
+} from '@mui/material'
 import React from 'react'
 import './ProductsListItem.scss'
 
@@ -22,6 +28,15 @@ const ProductsListItem = ({
                     <div className="cardinf">Type: {type}</div>
                     <div className="cardinf">Capacity: {capacity}GB</div>
                     <div className="price cardinf">Price: {price}$</div>
+                    <div className="cardinf product-quantity">
+                        <Button size="large" variant="outlined">
+                            -
+                        </Button>
+                        <TextField value="1" readonly size="small" />
+                        <Button size="large" variant="outlined">
+                            +
+                        </Button>
+                    </div>
                 </CardContent>
                 <CardActions className="wrap-btn">
                     <Button variant="contained">Add To Cart</Button>
