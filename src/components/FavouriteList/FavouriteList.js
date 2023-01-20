e2d2ff4068a9a64e435dd69121267f4a348a32a7
Oleg <Olegminenko2005@gmail.com>
2023-01-20 15:33:51 +0200

    upgrade

--- src/components/FavouriteList/FavouriteList.js
+++ src/components/FavouriteList/FavouriteList.js
@@ -3,6 +3,8 @@ import React from 'react'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import FavoriteIcon from '@mui/icons-material/Favorite'
 import productsArray, { getProductsObject } from 'utils/productsArray'
+import { spacing } from '@mui/system'
+import { Link } from 'react-router-dom'
 const FavouriteList = ({
     productsLike,
     productsObject = getProductsObject(productsArray),
@@ -12,36 +14,49 @@ const FavouriteList = ({
     const filteredArray = productsLikeArray.filter(
         (likeKey) => likeKey[1] != false
     )
+    const productsLikeObject = Object.fromEntries(filteredArray)
 
-    console.log(filteredArray)
-
-    return (
-        <div className="container liked">
-            {filteredArray.map((item) => (
-                <Card className="card-item">
-                    <CardContent className="content">
-                        <Button onClick={() => toggleLike(item[0])}>
-                            {productsLike[item[0]] ? (
-                                <FavoriteIcon></FavoriteIcon>
-                            ) : (
-                                <FavoriteBorderIcon></FavoriteBorderIcon>
-                            )}
-                        </Button>
-                        <div className="name">
-                            {productsObject[item[0]].title}
-                        </div>
-                        <div className="center">
-                            <img src={productsObject[item[0]].image}></img>
-                        </div>
-                        <div className="price">
-                            For one item -
-                            <strong>{productsObject[item[0]].price}</strong>
-                        </div>
-                    </CardContent>
-                </Card>
-            ))}
-        </div>
-    )
+    console.log(productsLikeObject)
+    if (filteredArray != 0) {
+        return (
+            <div className="container liked">
+                {Object.keys(productsLikeObject).map((productsLikeId) => (
+                    <Card className="card-item">
+                        <CardContent className="content">
+                            <Button onClick={() => toggleLike(productsLikeId)}>
+                                {productsLike[productsLikeId] ? (
+                                    <FavoriteIcon></FavoriteIcon>
+                                ) : (
+                                    <FavoriteBorderIcon></FavoriteBorderIcon>
+                                )}
+                            </Button>
+                            <div className="name">
+                                {productsObject[productsLikeId].title}
+                            </div>
+                            <div className="center">
+                                <img
+                                    src={productsObject[productsLikeId].image}
+                                ></img>
+                            </div>
+                            <div className="price">
+                                For one item -
+                                <strong>
+                                    {productsObject[productsLikeId].price}
+                                </strong>
+                            </div>
+                        </CardContent>
+                    </Card>
+                ))}
+            </div>
+        )
+    } else {
+        return (
+            <p>
+                <Link to="/products">Go to products</Link> Please add some
+                products
+            </p>
+        )
+    }
 }
 
 export default FavouriteList
